9e070e0f68c35d89fba84859e75df0de13a14377
YamadaTakanori <hal.yamada.takanori.2021@gmail.com>
2022-08-27 23:29:30 +0900

    メテオ

---
+++ sky_enemy.h
@@ -0,0 +1,72 @@
+#pragma once
+//=============================================================================
+//
+// モデル処理 [sky_enemy.h]
+// Author : 
+//
+//=============================================================================
+#pragma once
+
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+#define MAX_SKY_ENEMY		(10)					// プレイヤーの数
+
+#define	SKY_ENEMY_SIZE		(5.0f)				// 当たり判定の大きさ
+
+//*****************************************************************************
+// 構造体定義
+//*****************************************************************************
+struct SKY_ENEMY
+{
+	XMFLOAT3			pos;		// ポリゴンの位置
+	XMFLOAT3			rot;		// ポリゴンの向き(回転)
+	XMFLOAT3			scl;		// ポリゴンの大きさ(スケール)
+
+	XMFLOAT4X4			mtxWorld;	// ワールドマトリックス
+
+	BOOL				load;
+	DX11_MODEL			model;		// モデル情報
+
+	float				circle1_spd;		// 移動スピード
+	float				circle2_spd;
+	float				dir;		// 向き
+	float				size;		// 当たり判定の大きさ
+	int					shadowIdx;	// 影のIndex
+
+	BOOL				use;
+
+	// 階層アニメーション用のメンバー変数
+	INTERPOLATION_DATA	*tbl_adr;	// アニメデータのテーブル先頭アドレス
+	int					tbl_size;	// 登録したテーブルのレコード総数
+	float				move_time;	// 実行時間
+
+	// 親は、NULL、子供は親のアドレスを入れる
+	SKY_ENEMY				*parent;	// 自分が親ならNULL、自分が子供なら親のplayerアドレス
+
+	// クォータニオン
+	XMFLOAT4			quaternion;	// クォータニオン
+	XMFLOAT3			upVector;	// 自分が立っている所
+
+	float angle1;
+	float angle2;
+
+	float radius1;
+	float radius2;
+
+	int stay_count;
+	int move_count;
+
+	int spawn;
+};
+
+//*****************************************************************************
+// プロトタイプ宣言
+//*****************************************************************************
+HRESULT InitSkyEnemy(void);
+void UninitSkyEnemy(void);
+void UpdateSkyEnemy(void);
+void DrawSkyEnemy(void);
+
+SKY_ENEMY *GetSkyEnemy(void);
